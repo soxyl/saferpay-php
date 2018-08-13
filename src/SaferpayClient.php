d3b742f4afb80410b9571fbf20fdcfdb7bdb1a46
Samuel Wicky <samuel.wicky@avq.ch>
2018-08-13 11:33:34 +0200

    Project setup completed

---
+++ src/SaferpayClient.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Soxyl\Saferpay;
+
+class SaferpayClient
+{
+}
---
+++ tests/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class DummyTest extends TestCase
+{
+    public function testDummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}
