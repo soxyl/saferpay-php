e66bc1e0bf8960b91c97871a0102d0c9131e57a5
Samuel Wicky <samuel.wicky@avq.ch>
2018-08-14 13:11:01 +0200

    Cleanup

--- generate.sh
+++ generate.sh
@@ -1,11 +1,9 @@
-# Regenerate Client
+#!/bin/bash
 
-Run the following snippet to regenerate the client
-
-```
 docker run --rm --user 1000:1000 -v ${PWD}:/local openapitools/openapi-generator-cli generate \
     -i https://raw.githubusercontent.com/soxyl/saferpay-openapi/master/openapi.yaml \
     -g php \
     -o /local \
     -c /local/config.yaml
-```
+
+sed -i "s|proprietary|MIT|g" composer.json
